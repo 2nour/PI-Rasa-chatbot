3fc382742e76e0f6cd42acc064ae56f83050b632
medhedi77 <medezzine777@gmail.com>
2022-04-22 16:38:37 +0200

    Complaint Commit

--- actions/actions.py
+++ actions/actions.py
@@ -1,14 +1,15 @@
-from typing import Any, Text, Dict, List ## Datatypes
-
-from rasa_sdk import Action, Tracker  ##
+import random
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker  
 from rasa_sdk.executor import CollectingDispatcher
-
+from datetime import date
 
 from typing import Text, List, Any, Dict
 
-from rasa_sdk import Tracker, FormValidationAction
+from rasa_sdk import Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.types import DomainDict
+from database_connectivity import *
+
 
 
 class PossibleCreditAction(Action):
@@ -21,10 +22,338 @@ class PossibleCreditAction(Action):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) ->List[Dict[Text, Any]]:
-        ## somme totale : salaire + 0.25 * 12 
         salary = tracker.get_slot("salary")
         duration = tracker.get_slot("duration")
-        possiblecredit = int(salary) * 12 * int(duration) * 0.9 * 0.3
-        dispatcher.utter_message(text = "Possible credit amount is  " + str(possiblecredit) )
+        credit_type = tracker.get_slot("credit_type")
+        if (credit_type =="personal credit"):
+            credit = int(salary) * int(duration) * 12 * 0.25 * 0.9
+        elif (credit_type == "car credit"):
+            credit = int(salary) * int(duration) * 12 * 0.30 * 0.9
+        else:
+            credit = int(salary) * int(duration) * 12 * 0.40 * 0.9
+        
+        dispatcher.utter_message(text = "your possible credit amount is"+str(credit))
         return []
 
+class CreateAccountAction(Action):
+    def name(self) -> Text:
+        return "action_create_account"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        name = tracker.get_slot("name")
+        idd = tracker.get_slot("id")
+        cin= int(idd)
+        email = tracker.get_slot("email")
+        birthdate = tracker.get_slot("birthdate")
+        number = tracker.get_slot("number")
+        num = int(number)
+        address= tracker.get_slot("address")
+        login = tracker.get_slot("login")
+        password = tracker.get_slot("password")
+        account_type = tracker.get_slot("account_type")
+        today = date.today()
+        date_open = today.strftime("%Y-%m-%d")
+        balance = 0.0
+        RIB = get_rib()+1
+        a = verif_cin(cin)
+        b = verif_login(login)
+        c = verif_mail(email)
+        if(len(idd)!=8):
+            dispatcher.utter_message(text = "your id number is invalid ")
+        elif(len(a)!=0):
+            dispatcher.utter_message(text = "the id is allready used")
+        elif(len(b)!=0):
+            dispatcher.utter_message(text = "the login is allready used")
+        elif(len(c)!=0):
+            dispatcher.utter_message(text = "the login is allready used")
+        else:
+            #create_account(name, cin, email, birthdate, num, address, login, password, RIB, date_open , balance, account_type) 
+
+            dispatcher.utter_message(text = "your account is added successfully ")
+        return []
+
+class AccountTypeAction(Action):
+    def name(self) -> Text:
+        return "action_account_type"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "Savings account", "payload":'/savings_account_ty{"account_type":"savings account"}'})
+        buttons.append({"title": "Current account", "payload":'/current_account_ty{"account_type":"current account"}'})
+        buttons.append({"title": "Salary account", "payload":'/salary_account_ty{"account_type":"salary account"}'})
+    
+        dispatcher.utter_message(text="Please select the account type you desire", buttons = buttons)
+    
+        return []
+
+class CarCreditDurationAction(Action):
+    def name(self) -> Text:
+        return "action_car_credit_duration"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "5 years", "payload":'/get_duration5{"duration":"5"}'})
+        buttons.append({"title": "7 years", "payload":'/get_duration7{"duration":"7"}'})
+        buttons.append({"title": "10 years", "payload":'/get_duration10{"duration":"10"}'})
+ 
+        dispatcher.utter_message(text="Please select the credit duration you desire", buttons = buttons)
+    
+        return []
+
+class PersonalCreditDurationAction(Action):
+    def name(self) -> Text:
+        return "action_personal_credit_duration"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "5 years", "payload":'/get_duration5{"duration":"5"}'})
+        buttons.append({"title": "7 years", "payload":'/get_duration7{"duration":"7"}'})
+ 
+        dispatcher.utter_message(text="Please select the personal credit duration you desire", buttons = buttons)
+    
+        return []
+
+class RealCreditDurationAction(Action):
+    def name(self) -> Text:
+        return "action_real_estate_credit_duration"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "7 years", "payload":'/get_duration7{"duration":"7"}'})
+        buttons.append({"title": "10 years", "payload":'/get_duration10{"duration":"10"}'})
+        buttons.append({"title": "15 years", "payload":'/get_duration15{"duration":"15"}'})
+        buttons.append({"title": "20 years", "payload":'/get_duration20{"duration":"20"}'})
+ 
+        dispatcher.utter_message(text="Please select the credit duration you desire", buttons = buttons)
+    
+        return []
+
+class CreditTypeAction(Action):
+    def name(self) -> Text:
+        return "action_credit_type"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "Personal credit", "payload":'/personal_credit{"credit_type":"personal credit"}'})
+        buttons.append({"title": "Car credit", "payload":'/car_credit{"credit_type":"car credit"}'})
+        buttons.append({"title": "Real estate credit", "payload":'/real_estate_credit{"credit_type":"real estate credit"}'})
+    
+        dispatcher.utter_message(text="Please select the account type you desire", buttons = buttons)
+    
+        return []
+
+class CreateCreditAction(Action):
+    def name(self) -> Text:
+        return "action_create_credit"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        salary = tracker.get_slot("salary")
+        duration = tracker.get_slot("duration")
+        credit_type = tracker.get_slot("credit_type")
+        desired_amount = tracker.get_slot("credit_amount")
+        today = date.today()
+        date_sub = today.strftime("%Y-%m-%d")
+        if (credit_type =="personal credit"):
+            credit = int(salary) * int(duration) * 12 * 0.25 * 0.9
+        elif (credit_type == "car credit"):
+            credit = int(salary) * int(duration) * 12 * 0.30 * 0.9
+        else:
+            credit = int(salary) * int(duration) * 12 * 0.40 * 0.9
+        account_id=1
+        if (credit<int(desired_amount)): 
+            dispatcher.utter_message(text = "the desired credit amount is greater than the possible amount ")
+        else:
+            create_credit(date_sub, int(duration), int(desired_amount), credit_type, account_id,0 , 1)
+            dispatcher.utter_message(text = "operation done , the credit amount was addes to your balance")
+        return []
+
+class ShowBalanceAction(Action):
+    def name(self) -> Text:
+        return "action_show_balance"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        account_id =1
+        balance = show_balance(account_id)
+        dispatcher.utter_message(text = "Your current balance is: "+balance)
+        return []
+
+class CheckEarningsAction(Action):
+    def name(self) -> Text:
+        return "action_check_earnings"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        account_id =1                                      
+        cur = check_earnings(account_id)
+        for i in cur :
+            dispatcher.utter_message(text = i)
+        return []
+
+class TransferMOneyAction(Action):
+    def name(self) -> Text:
+        return "action_transfer_money"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        name = tracker.get_slot("reciever_name")
+        rib = tracker.get_slot("RIB")
+        amount = tracker.get_slot("transf_amount")
+        a = verif_transfer_info(name,rib)
+        b = verif_amount(amount,account_id)
+        account_id=1
+        if(len(a)==0):
+            dispatcher.utter_message(text = "wrong customer name or RIB")
+        elif(b==0):
+            dispatcher.utter_message(text = "insufficient balance")
+        else:
+            transfer_money(rib , amount, account_id)
+            dispatcher.utter_message(text = "Transaction went successfully")
+
+        return []
+
+class CloseAccountAction(Action):
+    def name(self) -> Text:
+        return "action_delete"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        today = date.today()
+        date_closed = today.strftime("%Y-%m-%d")
+        account_id=1
+        close_account(date_closed, account_id)
+        dispatcher.utter_message(text = "Your account was closed successfully")
+        return []
+
+class AskWhyAction(Action):
+    def name(self) -> Text:
+        return "action_ask_why"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "Switching Bank", "payload":'/swittching_bank{"reason":"swaitching bank "}'})
+        buttons.append({"title": "Poor Customer Services", "payload":'/poor_services{"reason":"poor customer services"}'})
+        buttons.append({"title": " High Charges and Fees", "payload":'/high_charges{"reason":"high charges and fees"}'})
+    
+        dispatcher.utter_message(text="Please select the reason why you want to delete your account", buttons = buttons)
+    
+        return []
+
+
+class TransactionsTypoeAction(Action):
+    def name(self) -> Text:
+        return "action_transactions_type"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "Last 5", "payload":'/last_five_trans{"transactions_type":"last five"}'})
+        buttons.append({"title": "All", "payload":'/all_trans{"transactions_type":"All "}'})
+    
+        dispatcher.utter_message(text="Please select the trasnactions you want to get", buttons = buttons)
+    
+        return []
+
+class ComplaintTypoeAction(Action):
+    def name(self) -> Text:
+        return "action_complaint_type"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        buttons = []
+        buttons.append({"title": "Server crash problem", "payload":'/transactions_typee{"complaint_type":"server crash problem}'})
+        buttons.append({"title": "Transactions problem", "payload":'/transactions_typee{"complaint_type":"transactions problem}'})
+        buttons.append({"title": "Sign_in problem", "payload":'/transactions_typee{"complaint_type":"sign_in problem}'})
+        buttons.append({"title": "Other problem", "payload":'/transactions_typee{"complaint_type":"other problem}'})
+    
+        dispatcher.utter_message(text="Please select the trasnactions you want to get", buttons = buttons)
+    
+        return []
+
+class CloseAccountAction(Action):
+    def name(self) -> Text:
+        return "action_create_complaint"
+
+    def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) ->List[Dict[Text, Any]]:
+        desc = tracker.get_slot("complaint_type")
+        rib = tracker.get_slot("rib")
+        problem = tracker.get_slot("problem")
+        ref = random.randint(367,5246)
+        if(len(verif_rib(int(rib)))==0):
+            dispatcher.utter_message(text = "Invalid RIB")
+        elif(desc == "other prtoblem"):
+            create_complaint(problem, rib, ref)
+            dispatcher.utter_message(text = "Complaint stored succesffully")
+        else:
+            create_complaint(desc, rib, ref)
+            dispatcher.utter_message(text = "Complaint stored succesffully")
+        return []
---
+++ database_connectivity.py
@@ -0,0 +1,96 @@
+from colorama import Cursor
+from django.db import connection
+import pymysql
+mydatabase = pymysql.connect(
+  host="localhost",
+  port=3308 ,
+  user="root",
+  password="",
+  database="rasadatabase"
+)
+
+mycursor = mydatabase.cursor()
+
+#mycursor.execute("CREATE TABLE Customers (id int AUTO_INCREMENT PRIMARY KEY, full_name VARCHAR(255) , cin int(8) , email varchar(255)  , birthdate DATETIME , phone_number int ,address VARCHAR(255) ,login VARCHAR(255), password varchar(255) NOT NULL)")
+#mycursor.execute("CREATE TABLE Account (id int AUTO_INCREMENT PRIMARY KEY, RIB int(16) NOT NULL,  date_opened DATETIME NOT NULL , date_closed DATETIME , balance float NOT NULL , account_type varchar(255) NOT NULL , customer_id int NOT NULL , CONSTRAINT FK FOREIGN KEY (customer_id) REFERENCES Customers(id) )")
+#mycursor.execute("CREATE TABLE Credit (id int AUTO_INCREMENT PRIMARY KEY,  date DATE NOT NULL , duration int  NOT NULL , ammount float NOT NULL , credit_type varchar(255) NOT NULL  , account_id int NOT NULL , CONSTRAINT FK FOREIGN KEY (account_id) REFERENCES  Account(id))")
+#mycursor.execute("CREATE TABLE Credit_card (id int AUTO_INCREMENT PRIMARY KEY, credit_card_number int NOT NULL , date_exp date NOT NULL , cvv int NOT NULL , pin_code int NOT NULL , account_id int NOT NULL , constraint FK FOREIGN KEY  (account_id) REFERENCES Account(id))")
+#mycursor.execute("CREATE TABLE Transactions (id int AUTO_INCREMENT PRIMARY KEY, date DATETIME NOT NULL , amount_of_transaction float NOT NULL , transaction_type varchar(255) NOT NULL , account_id int NOT NULL , external_account_id int NOT NULL ,constraint FK FOREIGN KEY  (account_id) REFERENCES Account(id), constraint FK1 FOREIGN KEY  (account_id) references  Account(id))" )
+#mycursor.execute("CREATE TABLE Reclamation(id int AUTO_INCREMENT PRIMARY KEY, description varchar(255) NOT NULL , rib int(16) NOT NULL , state varchar(255) DEFAULT "In progress" , ref_code int NOT NULL)")
+
+def verif_cin(cin):
+  a = mycursor.execute('SELECT cin from Customers where cin='+cin)
+  return a
+def verif_login(login):
+  a = mycursor.execute('SELECT login from Customers where login='+login)
+  return a
+def verif_mail(mail):
+  a = mycursor.execute('SELECT email from Customers where email='+mail)
+  return a
+def get_rib():
+  a = mycursor.execute('SELECT RIB from Account ORDER BY id DESC LIMIT 1')
+  return a  
+
+def create_account(fullname, cin, email, birthdate, number, address, login, password ,RIB, date_open, balance, account_type):
+  sql = 'INSERT INTO Customers(full_name, cin, email, birthdate, phone_number, address, login ,password) VALUES ("{0}","{1}", "{2}", "{3}", "{4}", "{5}" , "{6}" )'.format(fullname, cin, email, birthdate, number,  address, login, password)
+  mycursor.execute(sql)
+  sql= 'SELECT id FROM Customers ORDER BY id DESC LIMIT 1'
+  customer_id = mycursor.execute(sql)
+  sql = 'INSERT INTO Account(RIB, date_opened, balance, account_type, customer_id ) VALUES ("{7}", "{8}", "{9}", "{10}","{11}" )'.format(RIB,date_open, balance, account_type, customer_id)
+  mycursor.execute(sql)
+
+def create_credit(date, duration, amount, credit_type,  account_id , external_account_id, transacion_type):
+  sql = 'INSERT INTO Customers(date, duration, amount, credit_type, account_id) VALUES ("{0}","{1}", "{2}", "{3}", "{4}")'.format(date, duration, amount, credit_type, account_id)
+  mycursor.execute(sql)
+  sql = 'INSERT INTO Transactions(date, amount_of_transaction, transaction_type, account_id, external_account_id) VALUES ("{0}","{1}", "{2}", "{3}", "{4}")'.format(date, amount, transacion_type, account_id, external_account_id)
+  mycursor.execute(sql)
+  sql = 'SELECT balance from Account where id='+account_id
+  bal = mycursor.execute(sql)+amount
+  sql = 'UPDATE Account set balance='+bal+' where id='+account_id
+  mycursor.execute(sql)
+
+def show_balance(account_id):
+  sql = 'SELECT balance from Account where id='+account_id
+  bal = mycursor.execute(sql)
+  return bal
+
+def check_earnings(account_id):
+  sql = 'SELECT date , amount_of_transaction , transaction_type , full_name from Transactions T , Customer C , Account A where T.account_id='+account_id+'and A.id=T.external_account_id and C.id = A.customer_id LIMIt 5'
+  tran = mycursor.execute(sql)
+  return tran
+
+def verif_transfer_info(fullname,rib):
+  sql = 'SELECT full_name , RIB from Customers C , Account A where C.fullname='+fullname+' and A.RIB='+rib+' and C.id=A.customer_id'
+  a = mycursor.execute(sql)
+  return a
+
+def transfer_money(rib , amount, account_id):
+  sql = 'SELECT balance from Account where id='+account_id
+  bal = mycursor.execute(sql)-amount
+  sql = 'UPDATE Account set balance='+bal+' where id='+account_id
+  mycursor.execute(sql)
+  sql = 'SELECT balance from Account where RIB='+rib
+  bal = mycursor.execute(sql)+amount
+  sql = 'UPDATE Account set balance='+bal+' where RIB='+rib
+  mycursor.execute(sql)
+
+def close_account(date_close, account_id):
+  sql = 'Update Account set date_closed='+date_close+' where id='+account_id
+  mycursor.execute(sql)
+
+def verif_amount(amount,account_id):
+  sql= 'SELECT balance from account where id='+account_id
+  bal=mycursor.execute(sql)
+  if(amount>bal):
+    return 0
+  else:
+    return 1
+  
+def create_complaint(desc, rib, ref):
+  sql = 'INSERT INTO Reclamations(description, rib, ref_code) VALUES ("{0}","{1}", "{2}")'.format(desc, rib, ref)
+  mycursor.execute(sql)
+
+def verif_rib(rib):
+  sql = 'SELECT RIB From Account where RIB='+rib
+  r = mycursor.execute(sql) 
+  return r
